79607e43519920a36e8b660fc1993748b01e400b
Brey Rivera <breyr2021@gmail.com>
2025-01-27 21:08:28 -0500

    Refactor password generation to exclude special characters from charset (#26)

--- frontend/src/lib/helpers.ts
+++ frontend/src/lib/helpers.ts
@@ -1,6 +1,6 @@
 // generating temporary passwords for user creation
 export function generateTempPassword(length: number = 12): string {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let password = "";
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * charset.length);
