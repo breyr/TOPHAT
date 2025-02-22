4d9483be48722a9aa3d7fc1f4f0b08321f4aa721
Brey Rivera <breyr2021@gmail.com>
2025-02-22 15:10:33 -0500

    fix background color

--- frontend/src/pages/AllUserTopologiesPage.tsx
+++ frontend/src/pages/AllUserTopologiesPage.tsx
@@ -1,38 +1,38 @@
 import { useEffect, useState } from 'react';
-import { useAuth } from '../hooks/useAuth';
-import { User } from '../models/User';
 import type { PartialAppUser, Topology } from '../../../common/src/index';
 import TopologyCard from '../components/TopologyCard';
+import { useAuth } from '../hooks/useAuth';
+import { User } from '../models/User';
 
 export default function AllUserTopologiesPage() {
   const { token, user, authenticatedApiClient } = useAuth();
   const [users, setUsers] = useState<User[]>([]);
   const [topologies, setTopologies] = useState<Topology[]>([]);
-  
+
 
 
   const handleDelete = async (topologyId: number) => {
     if (!token) {
-        return;
+      return;
     }
     try {
-        const response = await authenticatedApiClient.deleteTopology(topologyId);
-        // update topologies list on success
-        setTopologies((prevTopologies) => prevTopologies.filter(topology => topology.id !== response.data?.topologyId));
+      const response = await authenticatedApiClient.deleteTopology(topologyId);
+      // update topologies list on success
+      setTopologies((prevTopologies) => prevTopologies.filter(topology => topology.id !== response.data?.topologyId));
     } catch (error) {
-        console.error('Error deleting topology:', error);
+      console.error('Error deleting topology:', error);
     }
-}
+  }
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-          const res = await authenticatedApiClient.getAllUsers();
-          const filteredUsers = res.data?.filter((appUser: PartialAppUser) => appUser.id !== user?.id);
-          const usersWithFlag = filteredUsers?.map((appUser: PartialAppUser) => User.fromDatabase(appUser));
-          setUsers(usersWithFlag || []);
+        const res = await authenticatedApiClient.getAllUsers();
+        const filteredUsers = res.data?.filter((appUser: PartialAppUser) => appUser.id !== user?.id);
+        const usersWithFlag = filteredUsers?.map((appUser: PartialAppUser) => User.fromDatabase(appUser));
+        setUsers(usersWithFlag || []);
       } catch (error) {
-          console.error('Error fetching users:', error);
+        console.error('Error fetching users:', error);
       }
     };
 
@@ -53,36 +53,36 @@ export default function AllUserTopologiesPage() {
 
 
   return (
-      <section className="pt-4 flex flex-col gap-y-4">
-        {users.length ? (
-          users.map((u) => {
-            const userTopologies = topologies.filter(
-              (topology) => topology.userId === u.id
-            );
-            return (
-              <div key={u.id} className="relative p-2">
-                <p className='absolute -top-1 left-4 bg-white px-2'>{u.email}</p>
-                <section className="flex flex-row flex-wrap gap-x-8 p-2 border rounded">  
-                  {userTopologies.length ? (
-                    userTopologies.map((topology) => (
-                      <TopologyCard
-                        key={topology.id}
-                        {...topology}
-                        onDelete={() => handleDelete(topology.id)}
-                        onArchive={() => console.log("Archive", topology.id)}
-                        readOnly = {true}
-                      />
-                    ))
-                  ) : (
-                    <p>No topologies for this user</p>
-                  )}
-                </section>
-              </div>
-            );
-          })
-        ) : (
-            <p>No users found.</p>
-        )}
+    <section className="pt-4 flex flex-col gap-y-4">
+      {users.length ? (
+        users.map((u) => {
+          const userTopologies = topologies.filter(
+            (topology) => topology.userId === u.id
+          );
+          return (
+            <div key={u.id} className="relative p-2">
+              <p className='absolute -top-1 left-4 bg-[#ffffff] px-2'>{u.email}</p>
+              <section className="flex flex-row flex-wrap gap-x-8 p-2 border rounded">
+                {userTopologies.length ? (
+                  userTopologies.map((topology) => (
+                    <TopologyCard
+                      key={topology.id}
+                      {...topology}
+                      onDelete={() => handleDelete(topology.id)}
+                      onArchive={() => console.log("Archive", topology.id)}
+                      readOnly={true}
+                    />
+                  ))
+                ) : (
+                  <p>No topologies for this user</p>
+                )}
+              </section>
+            </div>
+          );
+        })
+      ) : (
+        <p>No users found.</p>
+      )}
     </section>
   );
 }
