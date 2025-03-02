6d85bc20b5bc6fc77bfb94c9ba0b44996cf8b8eb
Brey Rivera <breyr2021@gmail.com>
2025-03-01 21:20:20 -0500

    update draggable item to check for userId

--- frontend/src/components/reactflow/overlayui/DraggableItem.tsx
+++ frontend/src/components/reactflow/overlayui/DraggableItem.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from "../../../hooks/useAuth";
 import { Device } from "../../../models/Device";
 
 export interface DraggableItemProps {
@@ -8,21 +9,31 @@ export interface DraggableItemProps {
 }
 
 const DraggableItem: React.FC<DraggableItemProps> = ({ nodeSvg, nodeType, deviceData, isUsed }) => {
+    const { user } = useAuth();
     const onDragStart = (event: React.DragEvent<HTMLDivElement>, nodeType: string) => {
         event.dataTransfer.setData("application/reactflow", JSON.stringify({ nodeType, deviceData }));
         event.dataTransfer.effectAllowed = "move";
     };
 
+    const isAvailableToUser = !isUsed || (deviceData.userId === user?.id);
+
     return (
         <div
-            className={`w-24 h-24 flex flex-col justify-center items-center transition-transform transform ${!isUsed ? 'hover:scale-105 cursor-pointer' : 'cursor-not-allowed'}`}
-            onDragStart={!isUsed ? (event) => onDragStart(event, nodeType) : undefined}
-            draggable={!isUsed}
+            className={`w-24 h-24 flex flex-col justify-center items-center transition-transform transform ${isAvailableToUser ? 'hover:scale-105 cursor-pointer' : 'cursor-not-allowed'}`}
+            onDragStart={isAvailableToUser ? (event) => onDragStart(event, nodeType) : undefined}
+            draggable={isAvailableToUser}
         >
             <div className="w-12 h-12 mb-2">
                 {nodeSvg}
             </div>
-            <p className={`text-sm text-gray-700 ${isUsed ? 'line-through' : ''}`}>{deviceData.name}</p>
+            <p className={`text-sm text-gray-700 ${isAvailableToUser ? 'line-through' : ''}`}>
+                {deviceData.name}
+            </p>
+            {deviceData.userId && deviceData.userId !== user?.id &&
+                <span className="block text-xs text-red-500">
+                    In use
+                </span>
+            }
         </div>
     );
 };
