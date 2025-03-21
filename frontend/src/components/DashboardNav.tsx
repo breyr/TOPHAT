c2fd6ebf6c576853418c3c8c005b3522590d4b9e
Brey Rivera <breyr2021@gmail.com>
2025-03-21 15:27:15 -0400

    color HAT blue

--- frontend/src/components/DashboardNav.tsx
+++ frontend/src/components/DashboardNav.tsx
@@ -1,7 +1,7 @@
 import { GraduationCap } from "lucide-react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
-import { useState } from "react";
 import SettingsModal from "./SettingsModal";
 
 export default function DashboardNav() {
@@ -21,13 +21,13 @@ export default function DashboardNav() {
         <nav className="w-full flex flex-row items-center justify-between border-b-2 py-2 px-2">
             <div className="flex flex-row gap-1 items-center">
                 <GraduationCap size={27} className="mx-2 pb-1" />
-                <h4 className="p-0 m-0 tracking-wider text-lg">TOPHAT</h4>
+                <h4 className="p-0 m-0 tracking-wider text-lg">TOP<span className="text-blue-500">HAT</span></h4>
             </div>
             <div className="flex flex-row items-center gap-2">
-            <h4 onClick={handleSettingsClick} className="hover:cursor-pointer hover:text-blue-400">Settings</h4>
-            <h4 onClick={logoutAndRedirect} className="hover:cursor-pointer hover:text-blue-400">Log Out</h4>
+                <h4 onClick={handleSettingsClick} className="hover:cursor-pointer hover:text-blue-400">Settings</h4>
+                <h4 onClick={logoutAndRedirect} className="hover:cursor-pointer hover:text-blue-400">Log Out</h4>
             </div>
-            <SettingsModal isOpen={isSettingsOpen} onClose={() => setIsSettingsOpen(false)} />                
+            <SettingsModal isOpen={isSettingsOpen} onClose={() => setIsSettingsOpen(false)} />
         </nav>
     )
 }
