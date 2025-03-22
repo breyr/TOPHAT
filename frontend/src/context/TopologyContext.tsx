d34dadc8093f57a3130eb3e9ee669522ae15abcf
Brey Rivera <breyr2021@gmail.com>
2025-03-22 19:47:14 -0400

    refactor: update DashboardNav layout and improve MultiSelect functionality with reinitialization handling

--- frontend/src/context/TopologyContext.tsx
+++ frontend/src/context/TopologyContext.tsx
@@ -8,6 +8,8 @@ interface TopologyContextProps {
     setIsSaving: (isSaving: boolean) => void;
     lastUpdated: string;
     setLastUpdated: (updatedAt: string) => void;
+    isReinintializingLinks: boolean;
+    setIsReinitializingLinks: (isReinitializingLinks: boolean) => void;
 }
 
 export const TopologyContext = createContext<TopologyContextProps | undefined>(undefined);
@@ -16,9 +18,10 @@ export const TopologyProvider: React.FC<{ children: React.ReactNode }> = ({ chil
     const [topologyData, setTopologyData] = useState<Topology | null>(null);
     const [isSaving, setIsSaving] = useState<boolean>(false);
     const [lastUpdated, setLastUpdated] = useState<string>("");
+    const [isReinintializingLinks, setIsReinitializingLinks] = useState<boolean>(false);
 
     return (
-        <TopologyContext.Provider value={{ topologyData, setTopologyData, isSaving, setIsSaving, lastUpdated, setLastUpdated }}>
+        <TopologyContext.Provider value={{ topologyData, setTopologyData, isSaving, setIsSaving, lastUpdated, setLastUpdated, isReinintializingLinks, setIsReinitializingLinks }}>
             {children}
         </TopologyContext.Provider>
     );
